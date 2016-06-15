b11f1614e128d42376140baab6661c6a28e927a9
Pradip_Kolhe <pradip.kolhe@raweng.com>
2016-06-15 10:22:45 +0530

    plugin added

--- blog/plugins/myplugin/index.js
+++ blog/plugins/myplugin/index.js
@@ -24,20 +24,29 @@ module.exports = function Myplugin() {
 
    Myplugin.serverExtends = function(app) {
        //var express = require('express');
-       //var nunjucks = require('nunjucks');
-       //
-       ////var app = express();
-       //
-       //app.get('/', function (req, res) {
-       //    console.log('inside server extends');
-       //    var blogPostQuery = Stack.ContentType('blog_post').Query();
-       //    blogPostQuery.toJSON().find().spread(function (result) {
-       //        console.log(result);
-       //        //res.send(result);
-       //        res.render('pages/home/test.html', { 'entry': result })
-       //    });
-       //});
+       var nunjucks = require('nunjucks');
 
+       //var app = express();
+
+       app.extends().get('/', function (req, res) {
+           console.log('inside server extends');
+           var blogPostQuery = Stack.ContentType('blog_post').Query();
+           blogPostQuery.toJSON().find().spread(function (result) {
+              // console.log(result);
+               //res.send(result);
+               //var homeQuery = Stack.ContentType('home').Query();
+               //homeQuery.toJSON().find().homEntry.only('button')
+               //console.log(homeQuery);
+               var entry = {
+                   entry: result
+               }
+               res.render('pages/home/test.html', entry)
+           });
+       });
+       function error(err) {
+           // err object
+           console.log(err);
+       };
 
    }
 
